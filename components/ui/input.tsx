c9cde9b7ae33822fafd681221e574e2d08bf6184
Roland Odenore - jsboy <rolandodenore@gmail.com>
2025-09-16 16:27:46 +0100

    disable eslint-rule

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
-
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
